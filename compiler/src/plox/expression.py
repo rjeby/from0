fb5120c99aea647e5acfcbf5ab4c723edd1306c9
rjeby <yassine.rjeb@grenoble-inp.org>
2026-02-12 16:41:54 +0100

    adding runtime errors for type checking

--- compiler/src/plox/expression.py
+++ compiler/src/plox/expression.py
@@ -2,6 +2,8 @@ from src.plox.token import TokenType
 from src.plox.token import Token
 from src.plox.token import LiteralValue
 
+# TODO: Define RuntimeError / ParseError ...
+
 
 class Expression:
     def __init__(self, x: int):
@@ -36,29 +38,41 @@ class Binary(Expression):
     def evaluate(self) -> LiteralValue:
         left = self.left.evaluate()
         right = self.right.evaluate()
-        if left == None or right == None:
-            # TODO: Handle Dynamic Typing
-            return None
         match self.operator.type:
             case TokenType.MINUS:
-                return float(left) - float(right)
+                if isinstance(left, float) and isinstance(right, float):
+                    return float(left) - float(right)
             case TokenType.STAR:
-                return float(left) * float(right)
+                if isinstance(left, float) and isinstance(right, float):
+                    return float(left) * float(right)
+                raise Exception("Operands must be Numbers")
             case TokenType.SLASH:
                 # TODO: Check for zero division
-                return float(left) / float(right)
+                if isinstance(left, float) and isinstance(right, float):
+                    return float(left) / float(right)
+                raise Exception("Operands must be Numbers")
             case TokenType.PLUS:
                 if isinstance(left, str) and isinstance(right, str):
                     return left + right
-                return float(left) + float(right)
+                if isinstance(left, float) and isinstance(right, float):
+                    return float(left) + float(right)
+                raise Exception("Operands must be Numbers or Strings")
             case TokenType.GREATER:
-                return float(left) > float(right)
+                if isinstance(left, float) and isinstance(right, float):
+                    return float(left) > float(right)
+                raise Exception("Operands must be Numbers")
             case TokenType.GREATER_EQUAL:
-                return float(left) >= float(right)
+                if isinstance(left, float) and isinstance(right, float):
+                    return float(left) >= float(right)
+                raise Exception("Operands must be Numbers")
             case TokenType.LESS:
-                return float(left) < float(right)
+                if isinstance(left, float) and isinstance(right, float):
+                    return float(left) < float(right)
+                raise Exception("Operands must be Numbers")
             case TokenType.LESS_EQUAL:
-                return float(left) <= float(right)
+                if isinstance(left, float) and isinstance(right, float):
+                    return float(left) <= float(right)
+                raise Exception("Operands must be Numbers")
             case TokenType.EQUAL_EQUAL:
                 return type(left) == type(right) and left == right
             case TokenType.BANG_EQUAL:
@@ -79,10 +93,9 @@ class Unary(Expression):
         right = self.right.evaluate()
         match self.operator.type:
             case TokenType.MINUS:
-                if right == None:
-                    # TODO: Handle Dynamic Typing
-                    return None
-                return -float(right)
+                if isinstance(right, float):
+                    return -float(right)
+                raise Exception("Operand must be a Number")
             case TokenType.BANG:
                 if right == None or (isinstance(right, bool) and right == False):
                     return True
